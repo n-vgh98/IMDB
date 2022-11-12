124a674c286b4b8efdd69de72d04d85c0fe6b18b
n-vgh98 <nazi.gh988@gmail.com>
2022-11-12 16:15:23 +0330

    create movie detail url,view,template

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movies.views import movies_list
+from movies.views import *
 
 
 urlpatterns = [
     path('', movies_list, name='movies_list'),
+    path('detail/<int:pk>', detail_movie, name='detail_movie'),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from movies.models import *
+
 
 def movies_list(request):
-    return render(request, 'movies/index.html')
+    movies = Movie.objects.filter(is_valid=True)
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail_movie(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movies/detail.html', {'movie': movie})
